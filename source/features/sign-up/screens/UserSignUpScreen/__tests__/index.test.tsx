613a56d15c1a2ee3b82908008e71a71a7a91d36c
Luiz Gustavo <luiz.matias@upe.br>
2022-08-13 20:23:34 -0300

    test(usersignupscreen): define tests scenarios

---
+++ source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
@@ -0,0 +1,11 @@
+describe('UserSignUpScreen', () => {
+  it.todo(
+    'given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required',
+  );
+  it.todo(
+    'given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen',
+  );
+  it.todo(
+    'given user is already sign ip, when enter in app, must be redirect to search pokemon list screen',
+  );
+});
