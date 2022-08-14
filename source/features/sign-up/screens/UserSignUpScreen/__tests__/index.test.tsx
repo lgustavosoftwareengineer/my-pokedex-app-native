4f7e6df4dfe74da69f6d90091043e1e373ec27f2
Luiz Gustavo <luiz.matias@upe.br>
2022-08-14 15:21:13 -0300

    test: implement first test scenario from user sign up screen

--- source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
+++ source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
@@ -1,7 +1,21 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react-native';
+
+import {CoreAppContainer} from 'source/core/components';
+
+import {UserSignUpScreen} from '..';
+
 describe('UserSignUpScreen', () => {
-  it.todo(
-    'given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required',
-  );
+  it('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', () => {
+    render(
+      <CoreAppContainer>
+        <UserSignUpScreen />
+      </CoreAppContainer>,
+    );
+    const signUpButton = screen.getByTestId('sign-up-button-test-id');
+
+    expect(signUpButton).toBeDisabled();
+  });
   it.todo(
     'given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen',
   );
