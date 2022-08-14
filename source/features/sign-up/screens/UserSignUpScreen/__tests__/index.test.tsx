488c3aa36e026c713296451c48ecb0e33e7616bc
Luiz Gustavo <luiz.matias@upe.br>
2022-08-14 15:27:11 -0300

    test(usersignupscreen): add verification if erro message is appearing below inputs

--- source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
+++ source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import {render, screen} from '@testing-library/react-native';
+import {fireEvent, render, screen} from '@testing-library/react-native';
 
 import {CoreAppContainer} from 'source/core/components';
 
 import {UserSignUpScreen} from '..';
 
 describe('UserSignUpScreen', () => {
-  it('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', () => {
+  it('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', async () => {
     render(
       <CoreAppContainer>
         <UserSignUpScreen />
       </CoreAppContainer>,
     );
+    const nameInput = screen.getByTestId('name-input-test-id');
+    const ageInput = screen.getByTestId('age-input-test-id');
     const signUpButton = screen.getByTestId('sign-up-button-test-id');
 
+    fireEvent.changeText(nameInput, undefined);
+    fireEvent.changeText(ageInput, undefined);
+
+    const inputsErrors = await screen.findAllByText('Este campo é requerido');
+    expect(inputsErrors).toHaveLength(2);
     expect(signUpButton).toBeDisabled();
   });
   it.todo(
