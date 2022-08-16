88c62010f4aaadbf18ce55eea980c531170e0761
Luiz Gustavo <luiz.matias@upe.br>
2022-08-15 21:42:41 -0300

    style: fix eslint issues

--- source/constants/DefaultNavigationOptions.ts
+++ source/constants/DefaultNavigationOptions.ts
@@ -1,5 +1,5 @@
-import { NativeStackNavigationOptions } from '@react-navigation/native-stack'
+import {NativeStackNavigationOptions} from '@react-navigation/native-stack';
 
 export const DefaultNavigationOptions = {
   headerShown: false,
-} as NativeStackNavigationOptions
+} as NativeStackNavigationOptions;
