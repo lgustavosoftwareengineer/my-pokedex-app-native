065f957fd3feab8bf0c324d29e98bb17178316b9
Luiz Gustavo <luiz.matias@upe.br>
2022-08-20 19:49:43 -0300

    test(usersignupscreen): finish second test

--- source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
+++ source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import {fireEvent, render, screen} from '@testing-library/react-native';
-
+import {faker} from '@faker-js/faker';
 import {CoreAppContainer} from 'source/core/components';
-
 import {UserSignUpScreen} from '..';
+import {useImagePicker} from 'source/core/hooks/useImagePicker';
+import {App} from 'source/app/App';
+
+jest.mock('source/core/hooks/useImagePicker');
+const mockedUseImagePicker = jest.mocked(useImagePicker);
 
 describe('UserSignUpScreen', () => {
   it('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', async () => {
+    mockedUseImagePicker.mockReturnValue([
+      {assets: [{uri: ''}]},
+      {
+        getImageFromCamera: jest.fn(),
+        getImageFromLibrary: jest.fn(),
+      },
+    ]);
+
     render(
       <CoreAppContainer>
         <UserSignUpScreen />
@@ -23,9 +35,33 @@ describe('UserSignUpScreen', () => {
     expect(inputsErrors).toHaveLength(3);
     expect(signUpButton).toBeDisabled();
   });
-  it.todo(
-    'given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen',
-  );
+  it('given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen', async () => {
+    mockedUseImagePicker.mockReturnValue([
+      {assets: [{uri: faker.image.avatar()}]},
+      {
+        getImageFromCamera: jest.fn(),
+        getImageFromLibrary: jest.fn(),
+      },
+    ]);
+    render(
+      <CoreAppContainer>
+        <App />
+      </CoreAppContainer>,
+    );
+    const nameInput = screen.getByTestId('name-input-test-id');
+    const ageInput = screen.getByTestId('age-input-test-id');
+    const signUpButton = screen.getByTestId('sign-up-button-test-id');
+
+    fireEvent.changeText(nameInput, faker.name.firstName());
+    fireEvent.changeText(ageInput, String(faker.datatype.number(99)));
+    expect(signUpButton).toBeEnabled();
+    fireEvent.press(signUpButton);
+
+    const searchPokemonListScreen = await screen.findByTestId(
+      'SearchPokemonsListScreen',
+    );
+    expect(searchPokemonListScreen).toBeDefined();
+  });
   it.todo(
     'given user is already sign ip, when enter in app, must be redirect to search pokemon list screen',
   );
