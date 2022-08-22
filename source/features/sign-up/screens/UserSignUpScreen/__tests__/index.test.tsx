293c59909813bce54183c84fdd6e06ee799f4445
Luiz Gustavo <luiz.matias@upe.br>
2022-08-21 23:32:26 -0300

    test(usersignup): back with describe block

--- source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
+++ source/features/sign-up/screens/UserSignUpScreen/__tests__/index.test.tsx
@@ -11,88 +11,90 @@ import {User} from 'source/types/user';
 jest.mock('source/core/hooks/useImagePicker');
 const mockedUseImagePicker = jest.mocked(useImagePicker);
 
-const fakerUser: User = {
-  name: faker.name.firstName(),
-  profileImage: faker.image.avatar(),
-  age: String(faker.datatype.number(99)),
-  isLogged: true,
-};
+describe('UserSignUpScreen', () => {
+  const fakerUser: User = {
+    name: faker.name.firstName(),
+    profileImage: faker.image.avatar(),
+    age: String(faker.datatype.number(99)),
+    isLogged: true,
+  };
 
-test('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', async () => {
-  mockedUseImagePicker.mockReturnValue([
-    {assets: [{uri: ''}]},
-    {
-      getImageFromCamera: jest.fn(),
-      getImageFromLibrary: jest.fn(),
-    },
-  ]);
+  test('given some sign up field empty, then the sign up button must be disabled and must appear a message inside in each input saying it is required', async () => {
+    mockedUseImagePicker.mockReturnValue([
+      {assets: [{uri: ''}]},
+      {
+        getImageFromCamera: jest.fn(),
+        getImageFromLibrary: jest.fn(),
+      },
+    ]);
 
-  render(
-    <CoreAppContainer>
-      <UserSignUpScreen />
-    </CoreAppContainer>,
-  );
-  const nameInput = screen.getByTestId('name-input-test-id');
-  const ageInput = screen.getByTestId('age-input-test-id');
-  const signUpButton = screen.getByTestId('sign-up-button-test-id');
+    render(
+      <CoreAppContainer>
+        <UserSignUpScreen />
+      </CoreAppContainer>,
+    );
+    const nameInput = screen.getByTestId('name-input-test-id');
+    const ageInput = screen.getByTestId('age-input-test-id');
+    const signUpButton = screen.getByTestId('sign-up-button-test-id');
 
-  fireEvent.changeText(nameInput, '');
-  fireEvent.changeText(ageInput, '');
+    fireEvent.changeText(nameInput, '');
+    fireEvent.changeText(ageInput, '');
 
-  const inputsErrors = await screen.findAllByText('Este campo é requerido');
-  expect(inputsErrors).toHaveLength(3);
-  expect(signUpButton).toBeDisabled();
-});
+    const inputsErrors = await screen.findAllByText('Este campo é requerido');
+    expect(inputsErrors).toHaveLength(3);
+    expect(signUpButton).toBeDisabled();
+  });
 
-test('given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen', async () => {
-  mockedUseImagePicker.mockReturnValue([
-    {assets: [{uri: fakerUser.profileImage}]},
-    {
-      getImageFromCamera: jest.fn(),
-      getImageFromLibrary: jest.fn(),
-    },
-  ]);
-  render(
-    <CoreAppContainer>
-      <App />
-    </CoreAppContainer>,
-  );
-  const nameInput = screen.getByTestId('name-input-test-id');
-  const ageInput = screen.getByTestId('age-input-test-id');
-  const signUpButton = screen.getByTestId('sign-up-button-test-id');
+  test('given sign up field are enabled, when clicked, user must be redirect to search pokemon list screen', async () => {
+    mockedUseImagePicker.mockReturnValue([
+      {assets: [{uri: fakerUser.profileImage}]},
+      {
+        getImageFromCamera: jest.fn(),
+        getImageFromLibrary: jest.fn(),
+      },
+    ]);
+    render(
+      <CoreAppContainer>
+        <App />
+      </CoreAppContainer>,
+    );
+    const nameInput = screen.getByTestId('name-input-test-id');
+    const ageInput = screen.getByTestId('age-input-test-id');
+    const signUpButton = screen.getByTestId('sign-up-button-test-id');
 
-  fireEvent.changeText(nameInput, fakerUser.name);
-  fireEvent.changeText(ageInput, fakerUser.age);
-  expect(signUpButton).toBeEnabled();
-  fireEvent.press(signUpButton);
+    fireEvent.changeText(nameInput, fakerUser.name);
+    fireEvent.changeText(ageInput, fakerUser.age);
+    expect(signUpButton).toBeEnabled();
+    fireEvent.press(signUpButton);
 
-  const searchPokemonListScreen = await screen.findByTestId(
-    'SearchPokemonsListScreen',
-  );
-  expect(searchPokemonListScreen).toBeDefined();
-});
+    const searchPokemonListScreen = await screen.findByTestId(
+      'SearchPokemonsListScreen',
+    );
+    expect(searchPokemonListScreen).toBeDefined();
+  });
 
-test('given user is already sign ip, when enter in app, must be redirect to search pokemon list screen', async () => {
-  act(() =>
-    useUser.setState({
-      user: fakerUser,
-    }),
-  );
-  mockedUseImagePicker.mockReturnValue([
-    {assets: [{uri: fakerUser.profileImage}]},
-    {
-      getImageFromCamera: jest.fn(),
-      getImageFromLibrary: jest.fn(),
-    },
-  ]);
-  render(
-    <CoreAppContainer>
-      <App />
-    </CoreAppContainer>,
-  );
+  test('given user is already sign ip, when enter in app, must be redirect to search pokemon list screen', async () => {
+    act(() =>
+      useUser.setState({
+        user: fakerUser,
+      }),
+    );
+    mockedUseImagePicker.mockReturnValue([
+      {assets: [{uri: fakerUser.profileImage}]},
+      {
+        getImageFromCamera: jest.fn(),
+        getImageFromLibrary: jest.fn(),
+      },
+    ]);
+    render(
+      <CoreAppContainer>
+        <App />
+      </CoreAppContainer>,
+    );
 
-  const searchPokemonListScreen = await screen.findByTestId(
-    'SearchPokemonsListScreen',
-  );
-  expect(searchPokemonListScreen).toBeDefined();
+    const searchPokemonListScreen = await screen.findByTestId(
+      'SearchPokemonsListScreen',
+    );
+    expect(searchPokemonListScreen).toBeDefined();
+  });
 });
